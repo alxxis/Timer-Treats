e42c7439c5c30b0e1be3e80de88d892a4ae95a39
alxxis <apgeronimo4@gmail.com>
2025-03-20 17:13:08 -0400

    worked on second page

--- src/components/TreatOption.jsx
+++ src/components/TreatOption.jsx
@@ -1,13 +1,13 @@
 import './components.css';
 import {useState} from 'react';
 
-export default function TreatOption({setIsPopped, setTime, setTreat, setDescription, time = 15, unit = "min", treat = "Sesame Bun", description="Soft bread topped with sesame seeds, often filled with beans, or sweet pastes"}){
+export default function TreatOption({setIsPopped, setTime, setTreat, setDescription, time = 15, unit = "m", treat = "Sesame Bun", description="Soft bread topped with sesame seeds, often filled with beans, or sweet pastes"}){
     const displayedTime = parseInt(time) >=60 ? time/60 : time;
     return(
         <div className="component-container" onClick={() => {setIsPopped(true); setTime(time); setTreat(treat); setDescription(description)}}>
             <div className="time-selection-container">{displayedTime}{unit}</div>
-            <p>image</p>
-            <p>{treat}</p>
+            <img src={`/${treat}.png`} width={"50%"} height={"50%"} style={{paddingTop:"10px"}}/>
+            <p style={{fontWeight:"300"}}>{treat}</p>
         </div>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,9 @@ export default function Home(){
             <link href="https://fonts.cdnfonts.com/css/bogart-trial" rel="stylesheet"/>
             <h3 className="timer-treats-title">Timer <i>Treats</i></h3>
             <h1 className="bake-title">Bake a sweet treat & focus</h1>
-            <img className="matcha-image-on-bake" src="/matcha-cake.png" width={60} height={60}/>
-            <img className="sesame-image-on-bake" src="/sesame-bun.png" width={60} height={50}/>
-            <img className="thai-image-on-bake" src="/Thai-Tea-Tiramisu.png" width={60} height={50}/>
+            <img className="matcha-image-on-bake" src="/Matcha Crepe Cake.png" width={60} height={60}/>
+            <img className="sesame-image-on-bake" src="/Sesame Bun.png" width={60} height={50}/>
+            <img className="thai-image-on-bake" src="/Thai Tea Tiramisu.png" width={60} height={50}/>
             <h4 className="home-desc">Work using our Asian dessert inspired <strong style={{color:"black"}}>focus timer.</strong></h4>
             <h4 className="home-desc" style={{paddingLeft:"60px", paddingRight:"60px"}}>If you end the focus session, your treat will <strong style={{color:"black"}}>burn</strong>!</h4>
             {/* <div className="start-focusing-button">
